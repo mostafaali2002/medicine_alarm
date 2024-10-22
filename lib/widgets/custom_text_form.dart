09c3b80ec625e8d518766953b17ed79e2052acbd
Mostafaali <mostafa@gmail.com>
2024-10-22 17:57:29 +0300

    create custom text form

---
+++ lib/widgets/custom_text_form.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:medicine_alarm/utils/app_style.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required this.preIcon,
+    required this.hint,
+    this.sufIcon,
+    required this.validator,
+  });
+  final IconData? preIcon, sufIcon;
+  final String hint;
+  final String? Function(String?)? validator;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: validator,
+      decoration: InputDecoration(
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Color(AppStyle.kSecondryColor))),
+          enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Color(AppStyle.kSecondryColor))),
+          hintText: hint,
+          suffixIcon: Icon(
+            sufIcon,
+            color: const Color(AppStyle.kSecondryColor),
+          ),
+          prefixIcon: Icon(
+            preIcon,
+            color: const Color(AppStyle.kSecondryColor),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(11),
+          )),
+    );
+  }
+}
--- lib/widgets/login_english_body.dart
+++ lib/widgets/login_english_body.dart
@@ -2,25 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:medicine_alarm/utils/app_images.dart';
 import 'package:medicine_alarm/utils/app_style.dart';
 import 'package:medicine_alarm/widgets/custom_button.dart';
+import 'package:medicine_alarm/widgets/custom_text_form.dart';
 
-class LoginEnglishBody extends StatelessWidget {
+class LoginEnglishBody extends StatefulWidget {
   const LoginEnglishBody({super.key});
 
   @override
+  State<LoginEnglishBody> createState() => _LoginEnglishBodyState();
+}
+
+class _LoginEnglishBodyState extends State<LoginEnglishBody> {
+  GlobalKey<FormState> formKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(Assets.takeMedicine),
-        const Text(
-          "With us you will never\nforget take a medicine",
-          style: AppStyle.styleMedium20,
-        ),
-        CustomButton(
-            text: "Login",
-            colorValue: AppStyle.kSecondryColor,
-            onTap: () {},
-            textStyle: AppStyle.styleRegular20)
-      ],
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          Image.asset(Assets.takeMedicine),
+          const Text(
+            "With us you will never\nforget take a medicine",
+            style: AppStyle.styleMedium20,
+          ),
+          const SizedBox(
+            height: 42,
+          ),
+          CustomTextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "This Field is Required";
+              }
+              return null;
+            },
+            hint: "Phone Number",
+            preIcon: Icons.phone,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomTextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "This Field is Required";
+              }
+              return null;
+            },
+            hint: "Password",
+            preIcon: Icons.lock,
+          ),
+          const SizedBox(
+            height: 47,
+          ),
+          CustomButton(
+              text: "Login",
+              colorValue: AppStyle.kSecondryColor,
+              onTap: () {
+                if (formKey.currentState!.validate()) {}
+              },
+              textStyle: AppStyle.styleRegular20),
+        ],
+      ),
     );
   }
 }
